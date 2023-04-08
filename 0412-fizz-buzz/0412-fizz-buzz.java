80db3b470e6a24ee26018ec2433bd5e1aebf5c76
Han Jaeyoon <jaeyoon.han97@gmail.com>
2023-04-09 03:07:19 +0900

    Time: 1 ms (99.81%), Space: 43.7 MB (43.79%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> arrList = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i%3 == 0 && i%5 == 0){
+                arrList.add("FizzBuzz");
+            } else if (i%3 == 0) {
+                arrList.add("Fizz");
+            } else if (i%5 == 0){
+                arrList.add("Buzz");
+            } else {
+                arrList.add(String.valueOf(i));
+            }
+        }
+        return arrList;
+    }
+}
