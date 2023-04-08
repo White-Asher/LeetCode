d5503c62ebedad337d42171bbbe06332287346c8
Han Jaeyoon <jaeyoon.han97@gmail.com>
2023-04-08 23:10:08 +0900

    Time: 741 ms (52.47%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
 
-select name as `Employee` from Employee as e where managerid is not null and salary >
-(select salary from Employee where id = e.managerId);
+# sub-query slow;
+# select name as `Employee` from Employee as e where managerid is not null and salary >
+# (select salary from Employee where id = e.managerId);
+
+# faster sol
+select e1.name AS Employee from Employee e1
+join Employee e2
+on e1.managerId = e2.id
+where e1.salary > e2.salary
