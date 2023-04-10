4752f589a0d0e83a8a8c80b3c9595d6991ae6ba4
Han Jaeyoon <jaeyoon.han97@gmail.com>
2023-04-10 23:39:49 +0900

    Time: 1461 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -4,3 +4,11 @@ select c.name as Customers from Customers as c
 left join Orders as o
 on c.id = o.customerId 
 where o.customerId is null;
+
+# other sol 1
+# SELECT A.Name from Customers A
+# WHERE NOT EXISTS (SELECT 1 FROM Orders B WHERE A.Id = B.CustomerId)
+
+# other sol 2
+# SELECT A.Name from Customers A
+# WHERE A.Id NOT IN (SELECT B.CustomerId from Orders B)
