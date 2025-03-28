9ea0015b3de207dff9cc42806c05bfbc8da5f338
Jae-yoon Han <dkxmpower999@naver.com>
2025-03-28 13:27:33 +0900

    Time: 0 ms (100%), Space: 40.4 MB (64.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
+        int[] dp = new int[n + 1];
         dp[1] = 1;
-        
         if(n >= 2) {
             dp[2] = 2;
         }
-        for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i - 2];
+        for(int i = 2; i < n; i++) {
+            dp[i + 1] = dp[i] + dp[i - 1];
         }
-        
         return dp[n];
     }
+
 }
